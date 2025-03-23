b63a53d36b934259502d3665abb2ff1242b7ff3b
nqttt2025 <tiennguyen047icloud@gmail.com>
2025-03-23 17:42:09 +0700

    implement neural_network

--- neural_network.py
+++ neural_network.py
@@ -1,15 +1,45 @@
+import os
+import time
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense
-# from tensorflow.keras.utils import to_categorical  # Import for one-hot encoding
+from tensorflow.keras.layers import Dense
+
+print(tf.version.VERSION)
 
 data = {
     'feature1': [0.1, 0.2, 0.3, 0.4, 0.5],
     'feature2': [0.5, 0.4, 0.3, 0.2, 0.1],
-    'label': [0, 0, 1, 1, 1]
+    'label': [0, 0, 1, 1, 1]  
 }
 
 df = pd.DataFrame(data)
-X = df[['feature1', 'feature2']].values
-# y = to_categorical(df['label'].values)  # One-hot encode the labels
+
+X = df[['feature1', 'feature2']].values 
+y = df['label'].values 
+
+model = Sequential()
+
+model.add(Dense(8, input_dim=2, activation='relu'))  
+model.add(Dense(1, activation='sigmoid'))  
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X, y, epochs=100, batch_size=1, verbose=1)
+
+test_data = np.array([[0.2, 0.4]])  
+
+prediction = model.predict(test_data)
+predicted_label = (prediction > 0.5).astype(int) 
+print(f"Predicted label: {predicted_label[0][0]}")
+model.summary()
+
+model_stored = "model"
+os.makedirs(model_stored, exist_ok=True)
+os.unlink(os.path.join(model_stored, 'my_model.keras'))
+
+time.sleep(2)
+print("This message is displayed after a 2-second delay.")
+
+model.save(os.path.join(model_stored, 'my_model.keras'))
